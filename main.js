7a60cb37cfe4d37fbeae83ca2863b72b9891e5ec
Tezslarichard <tezslarichard@gmail.com>
2025-01-05 14:50:01 +0100

    fuggveny bevezetese

--- main.js
+++ main.js
@@ -57,34 +57,35 @@ th3.colSpan = 2 // Az utolsó fejléc cella két cellát von össze
 th3.innerHTML = array1[0].th3ertek //megadjuk a fejléc tartalmát a tömbböl
 th_row.appendChild(th3)
 
+function rendetrable(){  //függvény bevezetése, nincs parametere
+    for (const futo of array) {
+        const table_row = document.createElement('tr') // Létrehozzuk az új sort
+        tbody.appendChild(table_row) // Hozzáadjuk a sort a tbody-hoz
 
-for (const futo of array) {
-    const table_row = document.createElement('tr') // Létrehozzuk az új sort
-    tbody.appendChild(table_row) // Hozzáadjuk a sort a tbody-hoz
+        const td1 = document.createElement('td') // létrehozunk egy cellát
+        td1.innerHTML = futo.Szerzo  //megadjuk mit tartalmaz
+        table_row.appendChild(td1) //hozzáadjuk a sorhoz
 
-    const td1 = document.createElement('td') // létrehozunk egy cellát
-    td1.innerHTML = futo.Szerzo  //megadjuk mit tartalmaz
-    table_row.appendChild(td1) //hozzáadjuk a sorhoz
+        const td2 = document.createElement('td') // létrehozunk egy cellát
+        td2.innerHTML = futo.csapat  //megadjuk mit tartalmaz
+        table_row.appendChild(td2)//hozzáadjuk a sorhoz
 
-    const td2 = document.createElement('td') // létrehozunk egy cellát
-    td2.innerHTML = futo.csapat  //megadjuk mit tartalmaz
-    table_row.appendChild(td2)//hozzáadjuk a sorhoz
+        const td3 = document.createElement('td')// létrehozunk egy cellát
+        td3.innerHTML = futo.Muvei  //megadjuk mit tartalmaz
+        table_row.appendChild(td3)//hozzáadjuk a sorhoz
 
-    const td3 = document.createElement('td')// létrehozunk egy cellát
-    td3.innerHTML = futo.Muvei  //megadjuk mit tartalmaz
-    table_row.appendChild(td3)//hozzáadjuk a sorhoz
 
-    
-    if (futo.muvei2) { //ha van td4 akkor  létrehozunk egy cellát,megadjuk mit tartalmaz majd hozzáadjuk a sorhoz
-        td4 = document.createElement('td')
-        td4.innerHTML = futo.muvei2
-        table_row.appendChild(td4) 
-    
-    } else {
-        // Ha nincs td4 akkor összevonom a cellát
-       td3.colSpan = "2"
+        if (futo.muvei2) { //ha van td4 akkor  létrehozunk egy cellát,megadjuk mit tartalmaz majd hozzáadjuk a sorhoz
+            td4 = document.createElement('td')
+            td4.innerHTML = futo.muvei2
+            table_row.appendChild(td4) 
+        
+        } else {
+            // Ha nincs td4 akkor összevonom a cellát
+           td3.colSpan = "2"
+        }
+
+
     }
-
-
 }
-
+rendetrable() // meghivom a fuggvenyemet
